9ddae011826777b836e2bfb8812f7a10dc51034a
Alex <Alexnguyen@coursera.com>
2019-12-03 15:30:29 +0700

    webapp

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 (function(){
   angular.module('webapp',[])
-  .controller('webappcontroller',webappcontroller);
-  
+  .controller('webappcontroller',webappcontroller)
+  .filter('Name',NameFilter);
+  webappcontroller.$inject = ['$scope','NameFilter'];
+  function webappcontroller ($scope,NameFilter){
+    $scope.blur = function(){
+      var message = $scope.textt;
+      $scope.textt_1 = NameFilter(message);
+    };
+    }
+    function NameFilter(){
+      return function(input){
+        input = input || "";
+        input = input.replace("you","Alex");
+        return input;
+      };
+  }
+
 
 })();
